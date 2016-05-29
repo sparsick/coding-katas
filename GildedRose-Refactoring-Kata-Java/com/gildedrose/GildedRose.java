c10a46cd8f07ca57c353c85e1005976af9478051
sparsick <sparsick@web.de>
2016-05-29 13:18:52 +0200

    refactoring: replace if-else construct by itemStrategyMap

--- GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRose.java
@@ -1,9 +1,21 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class GildedRose {
     Item[] items;
 
     ItemStrategy itemStrategy = new ItemStrategy();
+
+    static Map<String, IItemStrategy> itemStrategyMap = new HashMap<>();
+
+    static {
+        itemStrategyMap.put("Aged Brie", new AgedBrieItemStrategy());
+        itemStrategyMap.put("Sulfuras, Hand of Ragnaros", new SulfurasItemStrategy());
+        itemStrategyMap.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassItemStrategy());
+        itemStrategyMap.put("Conjured", new ConjuredItemStrategy());
+    }
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -11,17 +23,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if("Aged Brie".equals(items[i].name)) {
-                items[i] = itemStrategy.updateQualityForAgedBrieItem(items[i]);
-            } else if ("Sulfuras, Hand of Ragnaros".equals(items[i].name)) {
-                items[i] = itemStrategy.updateQualityForSulfurasItem(items[i]);
-            } else if("Backstage passes to a TAFKAL80ETC concert".equals(items[i].name)) {
-                items[i] = itemStrategy.updateQualityForBackstagePassItem(items[i]);
-            } else if("Conjured".equals(items[i].name)) {
-                items[i] = itemStrategy.updateQualityForConjuredItem(items[i]);
-            } else {
-                items[i] = itemStrategy.updateQualityForNormalItem(items[i]);
-            }
+            IItemStrategy iItemStrategy = itemStrategyMap.getOrDefault(items[i].name, new NormalItemStrategy());
+            items[i] = iItemStrategy.updateQuality(items[i]);
         }
     }
 }
