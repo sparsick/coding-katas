6cdf3399f2608cf38e20bb6a04c864f34da6eb8b
sparsick <sparsick@web.de>
2016-05-29 11:09:07 +0200

    add test for decreasing SellIn and quality cannot be negative

--- GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void decreaseSellDateAndQualityCannotBeNegative() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
