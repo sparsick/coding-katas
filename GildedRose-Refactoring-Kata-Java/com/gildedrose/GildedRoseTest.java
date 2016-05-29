bc852338749707c99cfee85ef446db19a328af26
sparsick <sparsick@web.de>
2016-05-29 11:16:22 +0200

    add test for decrease quality

--- GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRoseTest.java
@@ -10,10 +10,24 @@ public class GildedRoseTest {
     public void decreaseSellDateAndQualityCannotBeNegative() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("foo", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
     }
+
+    @Test
+    public void decreaseQuality() {
+        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+
 
 }
