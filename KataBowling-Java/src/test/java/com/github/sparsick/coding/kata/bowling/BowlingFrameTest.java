76c7e38490d5dbfd7a4507d4fc032e4cec629c88
sparsick <sparsick@web.de>
2017-07-04 16:28:57 +0200

    add check for frame if is used to save last roll for a spare

--- KataBowling-Java/src/test/java/com/github/sparsick/coding/kata/bowling/BowlingFrameTest.java
+++ KataBowling-Java/src/test/java/com/github/sparsick/coding/kata/bowling/BowlingFrameTest.java
@@ -1,6 +1,7 @@
 package com.github.sparsick.coding.kata.bowling;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 import org.junit.Test;
 
@@ -19,7 +20,6 @@ public class BowlingFrameTest {
         assertThat(totalSum).isEqualTo(2);
     }
 
-
     @Test
     public void totalSum_secondFailToKnockAll() {
         BowlingFrame frameUnderTest = new BowlingFrame("22");
@@ -28,56 +28,67 @@ public class BowlingFrameTest {
 
         assertThat(totalSum).isEqualTo(4);
     }
-    
+
     @Test
-    public void totalSum_failToKnockAllWithAMissAsFirstRoll(){
+    public void totalSum_failToKnockAllWithAMissAsFirstRoll() {
         BowlingFrame frameUnderTest = new BowlingFrame("-1");
-        
+
         int totalSum = frameUnderTest.totalSum();
-        
+
         assertThat(totalSum).isEqualTo(1);
     }
 
     @Test
-    public void totalSum_failToKnockAllWithTwoMissedRoll(){
+    public void totalSum_failToKnockAllWithTwoMissedRoll() {
         BowlingFrame frameUnderTest = new BowlingFrame("--");
-        
+
         int totalSum = frameUnderTest.totalSum();
-        
+
         assertThat(totalSum).isEqualTo(0);
     }
-    
+
     @Test
-    public void totalSum_strike(){
+    public void totalSum_strike() {
         BowlingFrame frameUnderTest = new BowlingFrame("X");
         frameUnderTest.nextFrame(new BowlingFrame("11"));
-        
+
         int totalSum = frameUnderTest.totalSum();
-        
+
         assertThat(totalSum).isEqualTo(12);
     }
-    
+
     @Test
-    public void totalSum_twoStrike(){
+    public void totalSum_twoStrike() {
         BowlingFrame nextFrameWithStrike = new BowlingFrame("X");
         nextFrameWithStrike.nextFrame(new BowlingFrame("11"));
         BowlingFrame frameUnderTest = new BowlingFrame("X");
         frameUnderTest.nextFrame(nextFrameWithStrike);
-        
+
         int totalSum = frameUnderTest.totalSum();
-        
+
         assertThat(totalSum).isEqualTo(21);
     }
-    
-    
+
     @Test
-    public void totalSum_spare(){
+    public void totalSum_spare() {
         BowlingFrame frameUnderTest = new BowlingFrame("3/");
         frameUnderTest.nextFrame(new BowlingFrame("11"));
-        
+
         int totalSum = frameUnderTest.totalSum();
-        
+
         assertThat(totalSum).isEqualTo(11);
+    }
+
+    @Test
+    public void lastRollAfterAFrameWithSpare() {
+        try {
+            new BowlingFrame("3");
+
+        } catch (Exception e) {
+            fail("No exception should be thrown.", e);
+
+        }
+
     }
 
 }
