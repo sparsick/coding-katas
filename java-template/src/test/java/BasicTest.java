625107c0a5ca06f64445db457a50dcddd61b7496
sparsick <sparsick@web.de>
2018-10-19 13:56:29 +0200

    update dependencies in java template

---
+++ java-template/src/test/java/BasicTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BasicTest {
+
+    @Test
+    void test(){
+        assertTrue(true);
+    }
+}
