03be3eee10c0e941ca4e2a664d22061b0e042e59
sparsick <sparsick@web.de>
2018-11-06 19:35:52 +0100

    add template for kotlin

---
+++ kotlin-template/src/test/kotlin/BasicTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.assertTrue
+
+internal class BasicTest {
+
+    @Test
+    fun test() {
+        assertTrue(true)
+    }
+}
